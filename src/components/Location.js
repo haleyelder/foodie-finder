77615e4ddba012917e93b8c06a1a9c4ca9a3b324
haley <haley.elder@live.com>
2023-08-09 20:16:57 -0700

    search bar and click to copy additions

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,13 +1,61 @@
+import { useState } from "react"
+
 export default function Location({locations}) {
+    const [search, setSearch] = useState('')
+    const [result, setResult] = useState(locations)
+
+    const filter = (e) => {
+        const keyword = e.target.value
+
+        if (keyword !== "") {
+            const results = locations.filter((location) => {
+                return location.name.toLowerCase().startsWith(keyword.toLowerCase())
+            });
+            setResult(results)
+        } else {
+            setResult(locations)
+        }
+        setSearch(keyword)
+    }
+
+    const handleClickCopyText = (e) => {
+        const copiedText = e.target.innerText    
+        navigator.clipboard.writeText(copiedText)
+        console.log(copiedText + ' copied!')
+    }
+
     return (
         <>
-            <div className="location-list">
             <div>Location</div>
+            <div className="search-container">
+                <input 
+                    type="search"
+                    value={search}
+                    onChange={filter}
+                    className="input"
+                    placeholder="search..."
+                />
+            </div>
+
+            <div className="user-list">
+                {result && result.length > 0 ? (
+                result.map((location) => (
+                    <li key={location.id} className="user">
+                    <span>{location.name}</span>
+                    </li>
+                ))
+                ) : (
+                <h1>No results found!</h1>
+                )}
+            </div>
+    
+            
+            <div className="location-list">
 
             {locations.map((location) => (
 
                 <div key={location.id} className="locations">
-                    <div className="location-name"><strong>{location.name}</strong> <span className="neighborhood"><em>- {location.neighborhood} </em></span></div>
+                    <div className="location-name" onClick={handleClickCopyText}><strong>{location.name}</strong> <span className="neighborhood"><em>- {location.neighborhood} </em></span></div>
                     
                     <div className="location-type"><em>{location.type}</em></div>
                     <div className="location-coords">location: {location.lat} - {location.long}</div>
