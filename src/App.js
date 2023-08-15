1f9c71d8eca861a5245bdf468d9cbe236ee2a8be
haley <haley.elder@live.com>
2023-08-14 20:28:01 -0700

    wip sorting, mostly working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 import Header from "./components/Header";
 import Location from "./components/Location";
-
 import data from './locations'
 
 function App({locations}) {
@@ -13,7 +12,7 @@ function App({locations}) {
       <div>
         <Header />
         <div className="main">
-        <MapContainer center={position} zoom={zoomLevel} scrollWheelZoom={true}>
+        {/* <MapContainer center={position} zoom={zoomLevel} scrollWheelZoom={true}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -29,9 +28,10 @@ function App({locations}) {
             </Popup>
             </Marker>
           ))} 
-        </MapContainer>
+        </MapContainer> */}
 
         <Location locations={data} className="location"/>
+
       </div>
       </div>
     </>
--- src/components/Location.js
+++ src/components/Location.js
@@ -1,71 +1,88 @@
-import { useState } from "react"
+import React, { useState } from "react";
 
-export default function Location({locations}) {
-    const [search, setSearch] = useState('')
-    const [result, setResult] = useState(locations)
-
-    const filter = (e) => {
-        const keyword = e.target.value
-
-        if (keyword !== "") {
-            const results = locations.filter((location) => {
-                return location.name.toLowerCase().startsWith(keyword.toLowerCase())
-            });
-            setResult(results)
-        } else {
-            setResult(locations)
-        }
-        setSearch(keyword)
-    }
-
-    const handleClickCopyText = (e) => {
-        const copiedText = e.target.innerText    
-        navigator.clipboard.writeText(copiedText)
-        console.log(copiedText + ' copied!')
-    }
-
-    return (
-        <>
-            <div>Location</div>
-            <div className="search-container">
-                <input 
-                    type="search"
-                    value={search}
-                    onChange={filter}
-                    className="input"
-                    placeholder="search..."
-                />
-            </div>
-
-            <div className="user-list">
-                {result && result.length > 0 ? (
-                result.map((location) => (
-                    <li key={location.id} className="user">
-                    <span>{location.name}</span>
-                    </li>
-                ))
-                ) : (
-                <h1>No results found!</h1>
-                )}
-            </div>
+export default function Location({ locations }) {
+  const [location, setLocation] = useState(locations)
+  
+  function onHeaderClick(e) {
+    let type = e.target.textContent.toLowerCase();
     
-            
-            <div className="location-list">
+    const sorted = [...locations].sort((a, b) =>
+    a[type] > b[type] ? 1 : b[type] > a[type] ? -1 : 0
+    );
+    setLocation(sorted);
+  }
+  
 
-            {locations.map((location) => (
 
-                <div key={location.id} className="locations">
-                    <div className="location-name" onClick={handleClickCopyText}><strong>{location.name}</strong> <span className="neighborhood"><em>- {location.neighborhood} </em></span></div>
-                    
-                    <div className="location-type"><em>{location.type}</em></div>
-                    <div className="location-coords">location: {location.lat} - {location.long}</div>
-                    <div className="location-visited">visited: {String(location.visited) === "true" ? "✓" : "false"}</div>
-                 
-                </div>
-            ))}
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th onClick={onHeaderClick}>Name</th>
+            <th onClick={onHeaderClick}>Neighborhood</th>
+            <th onClick={onHeaderClick}>Type</th>
+            <th onClick={onHeaderClick}>Visited</th>
+          </tr>
+        </thead>
 
+        {location.map((item) => (
+          <tbody key={item.id}>
+            <tr>
+              <td>{item.name}</td>
+              <td>{item.neighborhood}</td>
+              <td>{item.type}</td>
+              <td>{String(item.visited)}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
+
+      {/* search demo */}
+      {/* <div className="search-container">
+        <input
+          type="search"
+          value={search}
+          onChange={filter}
+          className="input"
+          placeholder="search..."
+        />
+      </div> */}
+
+      {/* <div className="user-list">
+        {result && result.length > 0 ? (
+          result.map((location) => (
+            <li key={location.id} className="user">
+              <span>{location.name}</span>
+            </li>
+          ))
+        ) : (
+          <h1>No results found!</h1>
+        )}
+      </div> */}
+      {/* 
+      <div className="location-list">
+        {locations.map((location) => (
+          <div key={location.id} className="locations">
+            <div className="location-name" onClick={handleClickCopyText}>
+              <strong>{location.name}</strong>{" "}
+              <span className="neighborhood">
+                <em>- {location.neighborhood} </em>
+              </span>
             </div>
-        </>
-    )
-}
 
+            <div className="location-type">
+              <em>{location.type}</em>
+            </div>
+            <div className="location-coords">
+              location: {location.lat} - {location.long}
+            </div>
+            <div className="location-visited">
+              visited: {String(location.visited) === "true" ? "✓" : "false"}
+            </div>
+          </div>
+        ))}
+      </div> */}
+    </>
+  );
+}
